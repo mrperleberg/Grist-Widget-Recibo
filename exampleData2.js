1cb80418443c7efa3e97fab79c784e4e98f85419
Murilo Perleberg <mrperleberg@inf.ufpel.edu.br>
2024-07-22 00:10:30 -0300

    Meu outro dado de exemplo

---
+++ exampleData2.js
@@ -0,0 +1,97 @@
+const exampleData2 = {
+  "id":2,
+  "Codigo":51373,
+  "Motorista":"Marcones Perleberg",
+  "Viagens":{
+    "tableId":"Viagens",
+    "rowIds":[4,5]
+  },
+  "Autor":{
+    "CEP":"96070-850",
+    "Cidade":"Pelotas",
+    "Email":"michele.perleberg@bol.com.br",
+    "Empresa":"Transportes Perleberg",
+    "Endereco":"Av. Fernando Osório, 6984",
+    "Estado":"RS",
+    "Phone":"(53) 98414-0297"
+  },
+  "Emissao":"2020-08-24T00:00:00.000Z",
+  "Due":"2020-09-24T00:00:00.000Z",
+  "Nota":"",
+  "References":{
+    "Autor":{
+      "CEP":"96070-850",
+      "Cidade":"Pelotas",
+      "Email":"michele.perleberg@bol.com.br",
+      "Empresa":"Transportes Perleberg",
+      "Endereco":"Av. Fernando Osório, 6984",
+      "Estado":"RS",
+      "Phone":"(53) 98414-0297"
+    },
+    "Codigo":51373,
+    "Due":"2020-09-24T00:00:00.000Z",
+    "Emissao":"2020-08-24T00:00:00.000Z",
+    "Motorista":{
+      "CEP":"96070-850",
+      "Cidade":"Pelotas",
+      "Endereco":"Fernando Osório, 6984",
+      "Estado":"RS",
+      "Motorista":"Marcones Perleberg",
+      "Placa":{
+        "tableId":"Veiculos",
+        "rowId":4
+      },
+      "Status":[
+        "Empregado"
+      ],
+      "TaxaComissao":0.18,
+      "Telefone":null,
+      "id":2
+    },
+    "Nota":"",
+    "Total":null,
+    "Viagens":
+    [
+      {
+        "CodigoRecibo":{
+          "tableId":"Recibos",
+          "rowId":2
+        },
+        "Comissao":null,
+        "Data_Carga":null,
+        "Data_Descarga":null,
+        "Destino":"POA_Dest",
+        "Motorista":"Marcones Perleberg",
+        "Origem":"PEL_OEM",
+        "Peso":1,
+        "Placa":"UmaPlaca",
+        "Preco":100,
+        "Produto2":"Clapping!",
+        "TaxaComissao":null,
+        "Valor":100,
+        "id":4
+      },
+      {
+        "CodigoRecibo":{
+          "tableId":"Recibos",
+          "rowId":2
+        },
+        "Comissao":null,
+        "Data_Carga":null,
+        "Data_Descarga":null,
+        "Destino":"RG_Dest",
+        "Motorista":"Marcones Perleberg",
+        "Origem":"PEL_OEM",
+        "Peso":3,
+        "Placa":null,
+        "Preco":39,
+        "Produto2":"Bravo",
+        "TaxaComissao":null,
+        "Valor":117,
+        "id":5
+      }
+    ],
+    "id":2
+  },
+  "Total":null
+};
